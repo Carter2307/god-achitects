a5fc11b294662e2f8761e27be610073e76e4df16
clemzouuu <clemkinoa@gmail.com>
2026-02-03 11:12:53 +0100

    back end working

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user.module';  
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule, 
+    UserModule  
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
